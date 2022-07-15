3bcf92b1017d610f4f3cd70ece110317f1e015ab
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-07-15 16:18:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 
 import React, { useState, useEffect} from "react";
 import './App.css'
-import axios from 'axios';
 
 const App = () => {
 
-const [news,setnews] = useState ();
+const [news,setnews] = useState ([]);
+const [error, setError] = useState(null);
 
-useEffect(async() => {
-  const result = await axios('https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0',
-  );
-
-  setnews(result.news);
-},[])
-
+useEffect(() => {
+  fetch('https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0')
+    .then(res => res.json())
+    .then(
+      (result) => {
+        
+        setnews(result);
+      },
+      (error) => {
+        setError(error);
+      }
+    )
+}, [])
+if (error) {
+  return <div>Error: {error.message}</div>;
+}  else {
   return (
    
-      <ul>
-      {news.map(item => (
-        <li key={item.created_at}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
-  );
+    <ul>
+        {news.map(item => (
+          <li key={item.created_at}>
+            {item.url} {item.title} {item.comment_text}
+          </li>
+        ))}
+      </ul>
+);
 }
+}
+
 export default App;
